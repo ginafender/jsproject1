970d03de372c3b525721d5e5cff6fc8b317289a6
ginafender <123966430+ginafender@users.noreply.github.com>
2024-10-07 15:31:41 -0400

    added starter stuff

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    this.style.display = 'none'; 
+    startGame(); 
+});
+
+function startGame() {
+    alert("Game is starting!");
+}
