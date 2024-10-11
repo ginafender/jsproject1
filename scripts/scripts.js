6c9f35c266db9cafcadcdb10b081056763b46b2c
ginafender <123966430+ginafender@users.noreply.github.com>
2024-10-11 14:13:51 -0400

    added logic for generating images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,36 @@
-title = document.getElementById("title");
+const title = document.getElementById('title');
 title.style.display = 'none';
-
+const images = document.getElementById('imagesContainer');
+images.style.display = 'none';
+const reset = document.getElementById('resetGame');
+reset.style.display = 'none';
 
 document.getElementById('playButton').addEventListener('click', function() {
     this.style.display = 'none'; 
     title.style.display = 'block';
+    images.style.display = 'block';
+    reset.style.display = 'block';
     startGame(); 
+    generateImages();
+});
+
+reset.addEventListener('click', function(){
+    startGame();
 });
 
 function startGame() {
-    alert("Game is starting!");
+    return
 }
+
+const imgCount = 16;
+const imgPath = 'images/green.png';
+function generateImages() {
+    for (i = 0; i < imgCount; i++){
+        let greenElement = document.createElement('img');
+        greenElement.src = imgPath;
+        greenElement.alt = `Image ${i + 1}`;
+        greenElement.classList.add('imagesStyles');
+        images.appendChild(greenElement);
+    }
+}
+
