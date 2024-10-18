c008dd4f8d70932b058e54f834cd294aa12e51f5
ginafender <123966430+ginafender@users.noreply.github.com>
2024-10-18 15:15:24 -0400

    finished project

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,54 +1,31 @@
 const title = document.getElementById('title');
 title.style.display = 'none';
-const images = document.getElementById('imagesContainer');
-images.style.display = 'none';
-// const reset = document.getElementById('resetGame');
-// reset.style.display = 'none';
+const imagesContainer = document.getElementById('imagesContainer');
+imagesContainer.style.display = 'none';
 
 document.getElementById('playButton').addEventListener('click', function() {
     this.style.display = 'none'; 
     title.style.display = 'block';
-    images.style.display = 'block';
-    // reset.style.display = 'block';
-    startGame(); 
-    // generateImages();
+    imagesContainer.style.display = 'grid';
+    hoverImages();
 });
 
-reset.addEventListener('click', function(){
-    startGame();
-});
+function hoverImages(){
+    let images = document.querySelectorAll('.imagesStyles');
+    images.forEach(img => {
+        let countryName = img.id;
+        let originalTitle = title.textContent;
+        const originalSrc = img.src; 
+        img.addEventListener('mouseover', function() {
+            img.src = `images/${countryName}IMG.jpg`;
+            title.textContent = countryName;
+        });
 
-function startGame() {
-    return
+        img.addEventListener('mouseout', function() {
+            setTimeout(function() {
+                img.src = originalSrc; 
+                title.textContent = originalTitle; 
+            }, 500);
+        });
+    });
 }
-
-// const flags = ['images/argentina.png', 'images/argentina.png', 'images/brazil.png', 'images/brazil.png',
-//             'images/chile.png', 'images/chile.png', 'images/colombia.png', 'images/colombia.png',
-//             'images/drj.png', 'images/drj.png', 'images/indonesia.png', 'images/indonesia.png',
-//             'images/sweden.png', 'images/sweden.png', 'images/switzerland.png', 'images/switzerland.png'];
-
-// const imgCount = flags.length;
-// const imgPath = 'images/green.png';
-// function generateImages() {
-//     for (i = 0; i < imgCount; i++) {
-//         let container = document.createElement('div');
-//         container.classList.add('imageContainer');
-
-//         let greenElement = document.createElement('img');
-//         greenElement.src = imgPath;
-//         greenElement.alt = `Image ${i + 1}`;
-//         greenElement.classList.add('greenStyles');
-
-//         let flagElement = document.createElement('img');
-//         flagElement.src = flags[i];
-//         flagElement.alt = flags[i];
-//         flagElement.classList.add('imagesStyles');
-
-//         container.appendChild(flagElement);
-//         container.appendChild(greenElement);
-        
-//         images.appendChild(container);
-//     }
-// }
-
-
